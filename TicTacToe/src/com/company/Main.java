eda71336fb5a2a4e29f16bcf5ff856c7ce19b5d6
ozan12341 <34062337+ozan12341@users.noreply.github.com>
2021-08-30 14:58:12 +0300

    Some arrengments

--- TicTacToe/src/com/company/Main.java
+++ TicTacToe/src/com/company/Main.java
@@ -1,27 +1,29 @@
 package com.company;
+
 import java.util.Scanner;
+
 public class Main {
 
-    private final static String LINE = "---------";
-    private final static String FIELD = "| %c %c %c |";
     private static boolean cont = true;
-    private static int counterEmpty = 0;
+
     public static void main(String[] args) {
+        int counterEmpty = 0;
+
         Scanner scanner = new Scanner(System.in);
 
         int[][] grid = new int[3][];
 
-        grid[0] = new int[]{' ', ' ', ' '};
-        grid[1] = new int[]{' ', ' ', ' '};
-        grid[2] = new int[]{' ', ' ', ' '};
+        for (int i = 0; i < 3; i++) {
+            grid[i] = new int[]{' ', ' ', ' '};
+        }
 
-        printGrid(grid);        // print empty board
+        printGrid(grid); // print empty board
         int counter = 0;
 
-        //request coordinates and win draw
+        // request coordinates and win draw
         while (cont) {
-            System.out.println("Enter the coordinates: ");                 // enter coordinates
-            String[] coordinates = scanner.nextLine().split(" ");       // coord before space - coord after space
+            System.out.println("Enter the coordinates: "); // enter coordinates
+            String[] coordinates = scanner.nextLine().split(" "); // coordinate before space - coordinate after space
             int x, y;
 
             try {
@@ -37,33 +39,32 @@ public class Main {
                     counterEmpty++;
                     if (counter % 2 == 0) {
                         grid[x - 1][y - 1] = 'X';
-                        printGrid(grid);
-                        playerHasWon(grid,counterEmpty);
-                        counter++;
-                        continue;
                     } else {
                         grid[x - 1][y - 1] = 'O';
-                        printGrid(grid);
-                        playerHasWon(grid,counterEmpty);
-                        counter++;
-                        continue;
                     }
+                    printGrid(grid);
+                    playerHasWon(grid, counterEmpty);
+                    counter++;
                 }
             } catch (NumberFormatException e) {
-                System.out.println("\nYou should enter numbers!");
+                System.out.println("You should enter numbers!");
             }
         }
     }
-    //print grid
+
+    // print grid
     private static void printGrid(int[][] grid) {
+        String LINE = "---------";
+        String FIELD = "| %c %c %c |\n";
+
         System.out.println(LINE);
         for (int[] row : grid) {
             System.out.printf(FIELD, row[0], row[1], row[2]);
-            System.out.println();
         }
         System.out.println(LINE);
     }
-    //win - draw
+
+    // win - draw
     private static void playerHasWon(int[][] grid, int counterEmpty) {
         //Check each row
         int draw = 0;
@@ -71,51 +72,44 @@ public class Main {
             if (grid[i][0] == grid[i][1] && grid[i][1] == grid[i][2] && grid[i][0] != ' ') {
                 if (grid[i][0] == 'X') {
                     System.out.println("X wins");
-                    cont = false;
-                    draw = 1;
                 } else {
                     System.out.println("O wins");
-                    cont = false;
-                    draw = 1;
                 }
+                cont = false;
+                draw = 1;
             }
         }
-        //Check each column
+
+        // Check each column
         for (int j = 0; j < 3; j++) {
             if (grid[0][j] == grid[1][j] && grid[1][j] == grid[2][j] && grid[0][j] != ' ') {
                 if (grid[0][j] == 'X') {
                     System.out.println("X wins");
-                    cont = false;
-                    draw = 1;
                 } else {
                     System.out.println("O wins");
-                    cont = false;
-                    draw = 1;
                 }
+                cont = false;
+                draw = 1;
             }
         }
-        //Check the diagonals
+        // Check the diagonals
         if (grid[0][0] == grid[1][1] && grid[1][1] == grid[2][2] && grid[0][0] != ' ') {
             if (grid[0][0] == 'X') {
                 System.out.println("X wins");
-                cont = false;
-                draw = 1;
             } else {
                 System.out.println("O wins");
-                cont = false;
-                draw = 1;
             }
+            cont = false;
+            draw = 1;
         }
         if (grid[2][0] == grid[1][1] && grid[1][1] == grid[0][2] && grid[2][0] != ' ') {
             if (grid[2][0] == 'X') {
                 System.out.println("X wins");
-                cont = false;
-                draw = 1;
             } else {
                 System.out.println("O wins");
-                cont = false;
-                draw = 1;
             }
+            cont = false;
+            draw = 1;
         }
         if (draw == 0 && counterEmpty == 9) {
             System.out.println("Draw");
